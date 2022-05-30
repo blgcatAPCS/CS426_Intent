75936e4e0f5df70a2eeddf8e62746d8f9e58c6b3
BLGCat <blgcat20@apcs.fitus.edu.vn>
2022-05-30 13:48:58 +0700

    add camera intent

--- Map_Camera/app/src/main/java/com/example/intent_mapandcamera/MainActivity.java
+++ Map_Camera/app/src/main/java/com/example/intent_mapandcamera/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.intent_mapandcamera;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private final int numPictureTaken = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(intent, numPictureTaken);
+        } catch (ActivityNotFoundException e){
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == numPictureTaken && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = (ImageView) findViewById(R.id.image_view_display);
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
